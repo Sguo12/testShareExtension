e1f4138ecd56efc31d55d8b46782043f22cd8653
Steven Guo <chineseweb.app@gmail.com>
2015-04-14 18:08:42 -0700

    - use subquery

---
+++ share/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  share
+//
+//  Created by Steven Guo on 4/14/15.
+//  Copyright (c) 2015 Tyanya Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
